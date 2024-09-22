ff7223fc5c4fe82a0cec21065b3860a63bc899eb
tserikawa <tkt.srkw@gmail.com>
2024-09-22 19:54:06 +0900

    コンポーネントのインポートとエクスポート

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
-function Profile() {
-  return <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />;
-}
-
-function Gallery(): JSX.Element {
-  return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
-  );
-}
+import Gallery from "./components/Gallery";
+import Profile from "./components/Profile";
 
 function App(): JSX.Element {
   return (
     <div>
       <Gallery />
+      <Profile />
     </div>
   );
 }
