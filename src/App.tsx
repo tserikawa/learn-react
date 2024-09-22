6a2b8ab756ccef5659e6578f6f87f5a74912317b
tserikawa <tkt.srkw@gmail.com>
2024-09-22 19:58:04 +0900

    JSXでのマークアップの記述

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,31 @@ import "./App.css";
 import Gallery from "./components/Gallery";
 import Profile from "./components/Profile";
 
+function Bio(): JSX.Element {
+  return (
+    <div>
+      <div className="intro">
+        <h1>Welcome to my website!</h1>
+      </div>
+      <p className="summary">
+        You can find my thoughts here.
+        <br />
+        <br />
+        <b>
+          And <i>pictures</i>
+        </b>{" "}
+        of scientists!
+      </p>
+    </div>
+  );
+}
+
 function App(): JSX.Element {
   return (
     <div>
       <Gallery />
       <Profile />
+      <Bio />
     </div>
   );
 }
